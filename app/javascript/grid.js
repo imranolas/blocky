521079125fddf3ec83ae1f47d892c654d8075271
imranolas <imranolas@gmail.com>
2016-03-04 09:33:56 +0000

    Traverse and update grid

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -2,6 +2,10 @@ export const COLOURS = ['red', 'green', 'blue', 'yellow'];
 const MAX_X = 10;
 const MAX_Y = 10;
 
+function copy(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
 export class Block {
     constructor (x, y) {
         this.x = x;
@@ -50,8 +54,75 @@ export class BlockGrid {
         return this;
     }
 
+    _purgeAndRender () {
+      document.getElementById('gridEl').innerHTML = "";
+      this.render();
+    }
+
+    _updateGrid (grid) {
+      this.grid = grid;
+      this._purgeAndRender();
+    }
+
+    _setBlock (grid, block) {
+      const {x, y} = block;
+      const newGrid = copy(grid);
+      newGrid[x][y] = block;
+      return newGrid;
+    }
+
+    _getGrid() {
+      return copy(this.grid);
+    }
+
+    _getCoords(x, y, grid) {
+      return grid[x] && grid[x][y]
+    }
+
+    _removedBlock (block) {
+      const newBlock = copy(block);
+      newBlock.colour = 'transparent';
+      return newBlock;
+    }
+
+    isTransparent(block={}) {
+      return block.colour === 'transparent';
+    }
+
+    isMatchingNeighbour(blockA, blockB) {
+      if (this.isTransparent(blockA) ||
+          this.isTransparent(blockA)) {
+        return false;
+      }
+
+      return (blockA && blockA.colour) &&
+             (blockB && blockB.colour) &&
+             (blockA.colour === blockB.colour);
+    }
+
+    traverseAndRemoveNeighbours(block, grid) {
+      console.log('traversing', block);
+      const coordTransformations = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+      const {x, y} = block;
+
+      const matchingNeighbours = coordTransformations
+        .map(([xTransform, yTransform]) => this._getCoords(x + xTransform, y + yTransform, grid))
+        .filter(neighbouringBlock => this.isMatchingNeighbour(block, neighbouringBlock))
+
+      // Nasty mutation. Ideally we'd return some state as a result of traversing
+      // but this avoids infinite recursive loops for now.
+      this._getCoords(x, y, grid).colour = 'transparent';
+
+      matchingNeighbours
+        .forEach(neighbouringBlock => {
+          this.traverseAndRemoveNeighbours(neighbouringBlock, grid);
+        });
+    }
+
     blockClicked (e, block) {
-        console.log(e, block);
+      const newGrid = this._getGrid();
+      this.traverseAndRemoveNeighbours(block, newGrid);
+      this._updateGrid(newGrid);
     }
 }
 
